8dcfddbb0de5696c9192c8ccd3bcee628805cf18
Jonas Moss <jonas.gjertsen@gmail.com>
2022-05-30 10:27:04 +0200

    Create tester_.R

---
+++ workspace/tester_.R
@@ -0,0 +1,9 @@
+library("semselector")
+set.seed(313)
+model <- "A =~ A1+A2+A3+A4+A5;
+          C =~ C1+C2+C3+C4+C5"
+n <- 200
+object <- lavaan::sem(model, psych::bfi[1:n, 1:10])
+selector <- semselector(object, n_reps = 50)
+options(warn=-1)
+
